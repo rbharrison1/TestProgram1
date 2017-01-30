f80c97c9ab4842a00cb41d56e62cd47dc6bd6818
rbharrison1 <rbharrison1@crimson.ua.edu>
2017-01-30 13:50:17 -0600

    ding dong

--- src/Main.java
+++ src/Main.java
@@ -4,42 +4,43 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        String filePath = "C:\\Users\\rharrison15\\Desktop\\TestFile.txt";
+        //String filePath = "C:\\Users\\rharrison15\\Desktop\\TestFile.txt";
+        String filePath = "C:\\Users\\ryneb\\Desktop\\TestFile.txt";
         String replace = filePath.replace("\\", "\\\\");
         BufferedReader br = null;
         FileReader fr = null;
         List<Standing> list = null;
 
-        try {
-            fr = new FileReader(replace);
 
-            br = new BufferedReader(fr);
+        fr = new FileReader(replace);
+        br = new BufferedReader(fr);
 
-            String sCurrentLine;
-            Standing team = null;
+        String sCurrentLine;
+        Standing team = new Standing();
+        System.out.println("2016 NL Central Standings");
+        System.out.println("\t\tW\tL\t%\t\tGB");
 
-            while ((sCurrentLine = br.readLine()) != null){
-                String[] data = sCurrentLine.split("\\|");
-
-                team.position = Integer.parseInt(data[0]);
-
+        while ((sCurrentLine = br.readLine()) != null){
+            String[] data = sCurrentLine.split("\\|");
+            team.position = Integer.parseInt(data[0]);
+            team.name = data[1];
+            team.wins = Integer.parseInt(data[2]);
+            team.losses = Integer.parseInt(data[3]);
+            team.pct = data[4];
+            team.gb = data[5];
                 
-                System.out.println(sCurrentLine);
-            }
+            System.out.println(team.position + ") " + team.name + "\t" + team.wins + "\t" + team.losses + "\t"
+                + team.pct + "\t" + team.gb);
         }
-        catch (IOException e)
-        {
 
-        }
-        finally {
-            fr.close();
-            br.close();
-        }
+        fr.close();
+        br.close();
+
     }
 
-    public class Standing {
+    public static class Standing {
         public int position;
-        public String team;
+        public String name;
         public int wins;
         public int losses;
         public String pct;
